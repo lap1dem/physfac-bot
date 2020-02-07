6dfaef594a765ffb94996dc637760ee8d466741e
lap1dem <vadym.bidula@gmail.com>
2020-02-07 18:12:31 +0200

    bf in civ random

--- implib.py
+++ implib.py
@@ -1,20 +1,20 @@
 from config import *
-import os
-import constants as c
 import json
-from telebot.types import InputMediaPhoto
-from datetime import date
-import psycopg2 as psql
-import shelve as sh
-from telebot.types import (
-    ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
-)
+import os
 import random
-import pandas as pd
-import numpy as np
-from openpyxl import load_workbook
+import shelve as sh
+from datetime import date
 
+import numpy as np
+import pandas as pd
+import psycopg2 as psql
+from openpyxl import load_workbook
+from telebot.types import (InlineKeyboardButton, InlineKeyboardMarkup,
+                           InputMediaPhoto, ReplyKeyboardMarkup)
+
+import constants as c
 import modules.navigation as nav
+
 
 def send_dev_msg(msg):
     bot.send_message(
