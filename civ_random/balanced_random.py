2b556161e16b3462102fef8ad2978d0d1e0d865f
lap1dem <vadym.bidula@gmail.com>
2020-08-22 18:01:15 +0300

    standart rating added

--- civ_random/balanced_random.py
+++ civ_random/balanced_random.py
@@ -3,32 +3,38 @@ import numpy as np
 import random
 from civ_random.generate_random_sets import generate_random_sets
 
-def load_rating(ncivs: int, bans):
-    rating = pd.read_csv('civ_random/rating.csv')
-    cols = rating.columns
+def load_rating(ncivs: int, bans, mode = 'standart'):
+    if mode = 'grades':
+        rating = pd.read_csv('civ_random/rating.csv')
+        cols = rating.columns
 
-    for col in cols[1:]:
-        for i in range(len(rating[col])):
-            try:
-                rat_val = rating[col].loc[i]
-                rat_val = rat_val.replace(" ", "").split(sep = ',')
-                rating[col].loc[i] = np.average([int(x) for x in rat_val])
-            except ValueError:
-                print('Incorrect value. Please, check the table.')
+        for col in cols[1:]:
+            for i in range(len(rating[col])):
+                try:
+                    rat_val = rating[col].loc[i]
+                    rat_val = rat_val.replace(" ", "").split(sep = ',')
+                    rating[col].loc[i] = np.average([int(x) for x in rat_val])
+                except ValueError:
+                    print('Incorrect value. Please, check the table.')
 
-    total_rat = []
+        total_rat = []
 
-    for i in range(len(rating[cols[0]])):
-        total_rat.append(
-                rating[cols[1]].loc[i] +
-                rating[cols[2]].loc[i] +
-                2 * rating[cols[3]].loc[i] +
-                rating[cols[4]].loc[i]
-        )
+        for i in range(len(rating[cols[0]])):
+            total_rat.append(
+                    rating[cols[1]].loc[i] +
+                    rating[cols[2]].loc[i] +
+                    2 * rating[cols[3]].loc[i] +
+                    rating[cols[4]].loc[i]
+            )
 
-    rating['AVERAGE'] = total_rat
-    rating = rating[~rating['Нація'].isin(bans)]
-    rating = rating.sort_values(by=['AVERAGE'], ascending=False).reset_index(drop=True)
+        rating['AVERAGE'] = total_rat
+        rating = rating[~rating['Нація'].isin(bans)]
+        rating = rating.sort_values(by=['AVERAGE'], ascending=False).reset_index(drop=True)
+
+    elif mode == 'standart':
+        rating = pd.read_csv('civ_random/rating.csv').rename(columns={0:'Місце', 1:'Нація'})
+        rating = rating[~rating['Нація'].isin(bans)].reset_index(drop=True)
+
     return rating
 
 def get_split_indices_from_probs(probs, length):
@@ -43,7 +49,7 @@ def get_split_indices_from_probs(probs, length):
 
 def balanced_random(pl_num:int, ncivs:int, bans):
     rand_civs = [[] for i in range(pl_num)]
-    rating = load_rating(ncivs, bans)
+    rating = load_rating(ncivs, bans, mode='standart')
     player_points = np.sum(np.arange(ncivs + 1))
     tier_sets, tier_probs, tiers = generate_random_sets(np.arange(1, ncivs + 1), ncivs, sum=player_points)
     split_inds = get_split_indices_from_probs(tier_probs, len(rating))
